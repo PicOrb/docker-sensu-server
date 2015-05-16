8afe01145fdaa429e857c00427ab47d8fd29d455
Ericson Cepeda <ubuntu@ip-172-31-46-166.ec2.internal>
2015-05-16 06:10:19 +0000

    Config on run time.

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,17 @@ FROM picorb/rabbitmq-base
 MAINTAINER Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
 
 # Redis
+ADD ./files/sensu.repo /etc/yum.repos.d/
 RUN yum install -y \
     redis \
-    sensu uchiwa \
     gcc g++ make \
     automake autoconf \
     curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel \
     ruby ruby-dev build-essential ruby-rdoc ruby-devel rubygems
+RUN yum install -y sensu uchiwa
 
 # Sensu server
-ADD ./files/sensu.repo /etc/yum.repos.d/
-ADD ./files/config.json /etc/sensu/
+#ADD ./files/config.json /etc/sensu/
 RUN /usr/bin/gem install sensu-plugin --no-rdoc --no-ri && \
     mkdir -p /etc/sensu/ssl && \
     cp /joemiller.me-intro-to-sensu/client_cert.pem /etc/sensu/ssl/cert.pem && \
@@ -31,7 +31,9 @@ ADD ./files/uchiwa.json /etc/sensu/
 RUN wget http://peak.telecommunity.com/dist/ez_setup.py;python ez_setup.py && \
     easy_install supervisor
 ADD files/supervisord.conf /etc/supervisord.conf
+ADD run.sh /tmp/sensu-run.sh
+RUN chmod +x /tmp/sensu-run.sh
 
 EXPOSE 3000 4369 4567 5672 5671 15672
 
-CMD ["/usr/bin/supervisord"]
+CMD ["/tmp/sensu-run.sh"]
---
+++ run.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+DASHBOARD_USER=${DASHBOARD_USER:-admin}
+DASHBOARD_PASS=${DASHBOARD_PASS:-sensu}
+SENSU_HOST=${SENSU_HOST:-localhost}
+RABBITMQ_HOST=${RABBITMQ_HOST:-localhost}
+REDIS_HOST=${REDIS_HOST:-localhost}
+SKIP_CONFIG=${SKIP_CONFIG:-}
+SENSU_CONFIG_URL=${SENSU_CONFIG_URL:-}
+SENSU_CLIENT_CONFIG_URL=${SENSU_CLIENT_CONFIG_URL:-}
+SENSU_CHECKS_CONFIG_URL=${SENSU_CHECKS_CONFIG_URL:-}
+
+if [ ! -z "$SENSU_CONFIG_URL" ] ; then
+    wget --no-check-certificate -O /etc/sensu/config.json $SENSU_CONFIG_URL
+else
+    if [ ! -e "/etc/sensu/config.json" ] ; then
+        cat << EOF > /etc/sensu/config.json
+{
+  "rabbitmq": {
+    "port": 5671,
+    "host": "$RABBITMQ_HOST",
+    "user": "sensu",
+    "password": "password",
+    "vhost": "/sensu",
+    "ssl": {
+      "cert_chain_file": "/etc/sensu/ssl/cert.pem",
+      "private_key_file": "/etc/sensu/ssl/key.pem"
+    }
+  },
+  "redis": {
+    "host": "$REDIS_HOST",
+    "port": 6379
+  },
+  "api": {
+    "host": "$SENSU_HOST",
+    "bind": "0.0.0.0",
+    "port": 4567
+  },
+  "dashboard": {
+    "host": "$SENSU_HOST",
+    "port": 8080,
+    "user": "$DASHBOARD_USER",
+    "password": "$DASHBOARD_PASS"
+  },
+  "handlers": {
+    "default": {
+      "type": "pipe",
+      "command": "true"
+    }
+  }
+}
+
+EOF
+    fi
+fi
+
+if [ ! -z "$SENSU_CLIENT_CONFIG_URL" ] ; then
+    wget --no-check-certificate -O /etc/sensu/conf.d/client.json $SENSU_CLIENT_CONFIG_URL
+else
+    if [ ! -e "/etc/sensu/conf.d/client.json" ] ; then
+    cat << EOF > /etc/sensu/conf.d/client.json
+{
+    "client": {
+      "name": "sensu-server",
+      "address": "localhost",
+      "subscriptions": [ "default", "common", "sensu" ]
+    },
+   "keepalive": {
+     "thresholds": {
+       "critical": 60
+     },
+     "refresh": 300
+   }
+}
+EOF
+    fi
+fi
+
+if [ ! -z "$SENSU_CHECKS_CONFIG_URL" ] ; then
+    wget --no-check-certificate -O /etc/sensu/conf.d/checks.json $SENSU_CHECKS_CONFIG_URL
+else
+    if [ ! -e "/etc/sensu/conf.d/checks.json" ] ; then
+    	cat << EOF > /etc/sensu/conf.d/checks.json
+{
+  "checks": {
+    "sensu-rabbitmq-beam": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/processes/check-procs.rb -p beam -C 1 -w 4 -c 5",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-rabbitmq-epmd": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/processes/check-procs.rb -p epmd -C 1 -w 1 -c 1",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-redis": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/processes/check-procs.rb -p redis-server -C 1 -w 4 -c 5",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-api": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/processes/check-procs.rb -p sensu-api -C 1 -w 4 -c 5",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    },
+    "sensu-dashboard": {
+      "handlers": [
+        "default"
+      ],
+      "command": "/etc/sensu/plugins/processes/check-procs.rb -p sensu-dashboard -C 1 -w 1 -c 1",
+      "interval": 60,
+      "occurrences": 2,
+      "refresh": 300,
+      "subscribers": [ "sensu" ]
+    }
+  }
+}
+EOF
+  fi
+fi
+
+/etc/init.d/uchiwa start
+/usr/bin/supervisord
